c2d83a149c023d27c95b1fd5baa6fec2b46e76f8
BhanuuPratapp <103886926+BhanuuPratapp@users.noreply.github.com>
2022-08-05 20:24:23 +0530

    form manipulations for mouse and lists

---
+++ Form_manipulations.js
@@ -0,0 +1,22 @@
+const ul = document.querySelector('.items');
+
+ul.firstElementChild.textContent = 'Hello';
+ul.firstElementChild.style.background = 'red';
+//ul.secondElementChild.textContent = 'Hello';
+ul.children[1].style.background = 'red';
+
+//ul.children[1].innerText = 'Brad';
+//ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+ e.preventDefault();
+ console.log(e.target.id);
+ document.querySelector('#my-form').style.background = '#ccc';
+
+ document.querySelector('body').classList.add('bg-dark');
+ document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+})
